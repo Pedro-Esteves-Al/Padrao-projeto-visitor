b9fe298d698f28731b54f3bed76c15b007083efc
Pedro-Esteves-Al <pedroestevesa@gmail.com>
2025-06-10 11:08:50 -0300

    Implementação dos casos de teste

--- Visitor/src/test/java/org/example/PessoaVisitorTest.java
+++ Visitor/src/test/java/org/example/PessoaVisitorTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,48 +33,60 @@ class PessoaVisitorTest {
     void deveExibirAlunoJSON() {
         Aluno aluno = new Aluno(1, "Ana", new Curso("Sistemas de Informação"));
 
-        PessoaVisitor visitor = new PessoaVisitor();
-        assertEquals("Aluno{matricula=1, nome='Ana', curso=Sistemas de Informação}", visitor.exibir(aluno));
+        JSONVisitor visitor = new JSONVisitor();
+        assertEquals("{\n" +
+                "matricula: 1\n" +
+                "nome: Ana\n" +
+                "curso: Sistemas de Informação\n" +
+                "}", visitor.exibir(aluno));
     }
 
     @Test
     void deveExibirProfessorJSON() {
         Professor professor = new Professor(1, "Maria", "Doutorado");
 
-        PessoaVisitor visitor = new PessoaVisitor();
-        assertEquals("Professor{matricula=1, nome='Maria', titulacao='Doutorado'}", visitor.exibir(professor));
+        JSONVisitor visitor = new JSONVisitor();
+        assertEquals("{\n" +
+                "matricula: 1\n" +
+                "nome: Maria\n" +
+                "titulacao: Doutorado\n" +
+                "}", visitor.exibir(professor));
     }
 
     @Test
     void deveExibirFuncionarioJSON() {
         Funcionario funcionario = new Funcionario(1, "Pedro", 5000.0f);
 
-        PessoaVisitor visitor = new PessoaVisitor();
-        assertEquals("Funcionario{codigo=1, nome='Pedro', salario=5000.0}", visitor.exibir(funcionario));
+        JSONVisitor visitor = new JSONVisitor();
+        assertEquals("{\n" +
+                "codigo: 1\n" +
+                "nome: Pedro\n" +
+                "salario: 5000.0\n" +
+                "}", visitor.exibir(funcionario));
     }
 
     @Test
     void deveExibirAlunoCSV() {
         Aluno aluno = new Aluno(1, "Ana", new Curso("Sistemas de Informação"));
 
-        PessoaVisitor visitor = new PessoaVisitor();
-        assertEquals("Aluno{matricula=1, nome='Ana', curso=Sistemas de Informação}", visitor.exibir(aluno));
+        CSVVisitor visitor = new CSVVisitor();
+        assertEquals("Matricula, Nome, Curso 1, Ana, Sistemas de Informação", visitor.exibir(aluno));
     }
 
     @Test
     void deveExibirProfessorCSV() {
         Professor professor = new Professor(1, "Maria", "Doutorado");
 
-        PessoaVisitor visitor = new PessoaVisitor();
-        assertEquals("Professor{matricula=1, nome='Maria', titulacao='Doutorado'}", visitor.exibir(professor));
+        CSVVisitor visitor = new CSVVisitor();
+        assertEquals("Matricula, Nome, Titulacao 1, Maria, Doutorado", visitor.exibir(professor));
     }
 
     @Test
     void deveExibirFuncionarioCSV() {
         Funcionario funcionario = new Funcionario(1, "Pedro", 5000.0f);
 
-        PessoaVisitor visitor = new PessoaVisitor();
-        assertEquals("Funcionario{codigo=1, nome='Pedro', salario=5000.0}", visitor.exibir(funcionario));
+        CSVVisitor visitor = new CSVVisitor();
+        assertEquals("Codigo, Nome, Salario 1, Pedro, 5000.0", visitor.exibir(funcionario));
     }
 
 }
